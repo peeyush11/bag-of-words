fa10e645802771ac1dfcf59aeb8b5f6776290fc8
peeyush11 <peeyushkumar11@gmail.com>
2024-05-02 12:48:36 +0200

    Update clustering_strategy_kmeans_opencv.hpp

--- src/clustering/clustering_strategy_kmeans_opencv.hpp
+++ src/clustering/clustering_strategy_kmeans_opencv.hpp
@@ -1,12 +1,6 @@
 #ifndef CPP_FINAL_PROJECT_CLUSTERING_CLUSTERING_STRATEGY_KMEANS_OPENCV_HPP_
 #define CPP_FINAL_PROJECT_CLUSTERING_CLUSTERING_STRATEGY_KMEANS_OPENCV_HPP_
 
-/**
- * @file clustering_strategy_kmeans_opencv.hpp
- *
- * @author Jan Quakernack
- * @version 1.0
- */
 
 #include "clustering_strategy.hpp"
 
